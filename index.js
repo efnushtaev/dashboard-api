b2e48908540630cfd5a2560ae0d5353bd2fee343
efnushtaev <efnushtaev@gmail.com>
2022-07-19 21:22:00 +0300

    add express

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import http from "http";
+import express from "express";
 
-const host = "127.0.0.1";
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hi!')
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Hi!");
 });
 
-server.listen(port, host, () => {
-  console.log(`Сервер запущен на ${host}:${port}`);
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
 });
