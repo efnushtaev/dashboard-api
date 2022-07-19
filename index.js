cf69b15566905d255ef014b781f28e2815350774
efnushtaev <efnushtaev@gmail.com>
2022-07-19 22:26:58 +0300

    middlewares

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
 app.get("/hello", (req, res) => {
-  res.send("Hi!");
+  throw new Error("errrrrrorrrrr");
+});
+
+app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(502).send('Something broked')
 });
 
 app.listen(port, () => {
