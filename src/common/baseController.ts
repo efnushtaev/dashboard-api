d16cc5a87136d22a9a5ae9f46c625dc84ea54fe7
efnushtaev <efnushtaev@gmail.com>
2022-10-28 18:43:24 +0300

    83 lesson

--- src/common/baseController.ts
+++ src/common/baseController.ts
@@ -1,6 +1,6 @@
 import { ILogger } from './../logger/logger.interface'
 import { Router, Response } from 'express'
-import { ControllerRoute } from './route.interface'
+import { IControllerRoute } from './route.interface'
 import { injectable } from 'inversify'
 import 'reflect-metadata'
 
@@ -29,12 +29,13 @@ export abstract class BaseController {
     return this.send<T>(res, 200, message)
   }
 
-  protected bindRoutes(routes: ControllerRoute[]) {
+  protected bindRoutes(routes: IControllerRoute[]) {
     for (const route of routes) {
       this.logger.log(`[${route.method}] ${route.path}`)
+      const middleware = route.middlewares?.map((m) => m.execute.bind(m))
       const handler = route.func.bind(this)
-
-      this.router[route.method](route.path, handler)
+      const pipeline = middleware ? [...middleware, handler] : handler
+      this.router[route.method](route.path, pipeline)
     }
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,4 @@
+import { ValidateMiddleware } from './../common/validate.middleware'
 import { IUserService } from './users.service.interface'
 import { UserRegisterDto } from './dto/user-register.dto'
 import { IUserController } from './users.interface'
@@ -19,7 +20,12 @@ export class UserController extends BaseController implements IUserController {
   ) {
     super(loggerService)
     this.bindRoutes([
-      { path: '/register', method: 'post', func: this.register },
+      {
+        path: '/register',
+        method: 'post',
+        func: this.register,
+        middlewares: [new ValidateMiddleware(UserRegisterDto)],
+      },
       { path: '/login', method: 'post', func: this.login },
     ])
   }
