01078173b535fa038d53d9e272e44ffacd6e3a5e
efnushtaev <efnushtaev@gmail.com>
2022-11-01 17:53:14 +0300

    lesson 86

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import { TYPES } from './types'
 import { Server } from 'http'
 import express, { Express } from 'express'
 
-import { UserController } from './users/users.controller'
-import { ExeptionFilter } from './errors/exeption.filter'
 import { ILogger } from './logger/logger.interface'
 import { inject, injectable } from 'inversify'
 import bodyParser from 'body-parser'
 import 'reflect-metadata'
+import { IExeptionFilter } from './errors/exeption.filter.interface'
+import { IConfigService } from './config/config.service.interface'
+import { UserController } from './users/users.controller'
+import { PrismaService } from './database/prisma.service'
 
 @injectable()
 export class App {
@@ -18,13 +20,12 @@ export class App {
   constructor(
     @inject(TYPES.ILogger) private logger: ILogger,
     @inject(TYPES.UserController) private userController: UserController,
-    @inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
+    @inject(TYPES.ExeptionFilter) private exeptionFilter: IExeptionFilter,
+    @inject(TYPES.ConfigService) private configService: IConfigService,
+    @inject(TYPES.PrismaService) private prismaServie: PrismaService,
   ) {
     this.app = express()
     this.port = 8001
-    this.logger = logger
-    this.userController = userController
-    this.exeptionFilter = exeptionFilter
   }
 
   private useMiddleware(): void {
@@ -43,6 +44,7 @@ export class App {
     this.useMiddleware()
     this.useRoutes()
     this.useExeptionFilters()
+    this.prismaServie.connect()
     this.server = this.app.listen(this.port)
     this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
   }
